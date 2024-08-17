993963f117c9c03d5eeb4f950cb17df9cd599794
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2024-08-16 22:51:52 -0300

    removing commented code

--- TopGithubRepos/Packages/app-coordinator/Sources/app-coordinator/AppDelegate/AppDelegate.swift
+++ TopGithubRepos/Packages/app-coordinator/Sources/app-coordinator/AppDelegate/AppDelegate.swift
@@ -22,20 +22,6 @@ open class AppDelegate: UIResponder, UIApplicationDelegate, AppCoordinatorProtoc
         return true
     }
     
-    // MARK: UISceneSession Lifecycle
-    
-//    public func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-//        // Called when a new scene session is being created.
-//        // Use this method to select a configuration to create the new scene with.
-//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-//    }
-    
-//    public func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-//        // Called when the user discards a scene session.
-//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-//    }
-    
     // MARK: - RootWindow
     open func setupRootWindow() {
         let window = self.window ?? UIWindow(frame: UIScreen.main.bounds)
